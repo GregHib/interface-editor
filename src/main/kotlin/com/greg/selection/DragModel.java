599563bc3e04a10dcba730de5f76b25fe2fe7c4f
Greg <gregoryhib@gmail.com>
2017-11-23 09:53:18 +0000

    Missing files

---
+++ src/main/kotlin/com/greg/selection/DragModel.java
@@ -0,0 +1,16 @@
+package com.greg.selection;
+
+public class DragModel {
+
+    Double sceneX;
+    Double sceneY;
+    Double translateX;
+    Double translateY;
+
+    public DragModel(double sceneX, double sceneY, double translateX, double translateY) {
+        this.sceneX = sceneX;
+        this.sceneY = sceneY;
+        this.translateX = translateX;
+        this.translateY = translateY;
+    }
+}
---
+++ src/main/kotlin/com/greg/widget/WidgetClass.java
@@ -0,0 +1,4 @@
+package com.greg.widget;
+
+public class WidgetClass {
+}
